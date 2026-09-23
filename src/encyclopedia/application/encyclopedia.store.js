3e73158c433099733141e3fa1c2cdcd63e7b2f9f
GZ-99 <gabrielvilchezvite@gmail.com>
2026-09-23 15:13:05 -0500

    chore: add encyclopedia.store.js

---
+++ src/encyclopedia/application/encyclopedia.store.js
@@ -0,0 +1,30 @@
+import {EncyclopediaApi} from "@/encyclopedia/infrastructure/encyclopedia-api.js";
+import {ResultAssembler} from "@/encyclopedia/infrastructure/result.assembler.js";
+import {reactive} from "vue";
+
+const encyclopediaApi = new EncyclopediaApi();
+const resultAssembler = new ResultAssembler();
+
+export const encyclopediaStore = reactive({
+    results: [],
+    errors: [],
+    currentResult: null,
+
+    async loadResults() {
+        this.errors = [];
+        try {
+            const response = await encyclopediaApi.getFeaturedSpecies();
+            this.results = ResultAssembler.toEntitiesFromResponse(response);
+            this.currentResult = this.results[0] || null;
+            return this.results;
+        } catch(message) {
+            this.errors.push(message);
+            this.results = [];
+            this.currentResult = null;
+            return [];
+        }
+    },
+    setCurrentResult(result) {
+        this.currentResult = result;
+    }
+});
