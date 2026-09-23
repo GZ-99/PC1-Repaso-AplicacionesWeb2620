3fe037079a09e1081d2f9883ddfad714b6b85fb7
GZ-99 <gabrielvilchezvite@gmail.com>
2026-09-23 17:38:26 -0500

    chore: add result-list.vue

--- src/encyclopedia/presentation/components/result-list.vue
+++ src/encyclopedia/presentation/components/result-list.vue
@@ -1,9 +1,28 @@
 <script setup>
+import ResultItem from "./result-item.vue";
+import {Result} from "@/encyclopedia/domain/model/result.entity.js";
 
+const props = defineProps({
+  results: {
+    type: Array,
+    required: true,
+    default: () => []
+  }
+});
+const emit = defineEmits(["result-shared"]);
+const handleResultShared = (resultId) => {
+  emit("result-shared", resultId);
+}
 </script>
 
 <template>
-
+  <div
+      v-for="result in props.results"
+      :key="result.getId()">
+    <result-item
+        :result="result"
+        @result-shared="handleResultShared"/>
+  </div>
 </template>
 
 <style scoped>
