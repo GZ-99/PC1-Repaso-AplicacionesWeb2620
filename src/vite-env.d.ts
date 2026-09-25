20f193e6bfde7a46f98e40b279ed2547fb9a5227
GZ-99 <gabrielvilchezvite@gmail.com>
2026-09-25 17:29:57 -0500

    feat(srd): update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,7 +2,6 @@ interface ImportMetaEnv {
     readonly VITE_ENCYCLOPEDIA_API_URL: string;
     readonly VITE_LOGO_API_URL: string;
     readonly VITE_LOGO_PUBLISHABLE_API_KEY: string;
-    readonly VITE_TOP_HEADLINES_ENDPOINT_PATH: string;
     readonly VITE_PRIME_UI_LICENSE_KEY: string;
 }
 
